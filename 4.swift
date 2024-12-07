641da1640b7cc6e929bd1976dc2487400f588e61
Loïs Bilat <git@billotais.ch>
2024-12-07 10:45:18 +0100

    day 4 finished

--- 4.swift
+++ 4.swift
@@ -12,7 +12,7 @@ let diagonal_left_pattern = try? Regex("X(.|\n){\(move-1)}M(.|\n){\(move-1)}A(.|
 
 var total_1 = 0
 
-func findOverlappingMatches(in text: String, regex: Regex<AnyRegexOutput>) -> [String] {
+func findOverlappingMatches(in text: String, regex: Regex<AnyRegexOutput>, valid_lines: [Int]) -> [String] {
     var matches: [String] = []
     var searchIndex = text.startIndex
     
@@ -22,7 +22,7 @@ func findOverlappingMatches(in text: String, regex: Regex<AnyRegexOutput>) -> [S
             // Check if match is valid, i.e. either on one line, or 4 lines
             let value = String(text[match.range]) 
             let rows = value.split(separator: "\n").count
-            if rows == 4 || rows == 1 {
+            if valid_lines.contains(rows) {
                 matches.append(value)
                 searchIndex = text.index(after: match.range.lowerBound)
             } else {
@@ -35,19 +35,26 @@ func findOverlappingMatches(in text: String, regex: Regex<AnyRegexOutput>) -> [S
     return matches
 }
 
-total_1 = total_1 + findOverlappingMatches(in: input, regex: horizontal_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: input, regex: vertical_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: input, regex: diagonal_right_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: input, regex: diagonal_left_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: horizontal_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: vertical_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: diagonal_right_pattern!).count
-total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: diagonal_left_pattern!).count
+total_1 = total_1 + findOverlappingMatches(in: input, regex: horizontal_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: input, regex: vertical_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: input, regex: diagonal_right_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: input, regex: diagonal_left_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: horizontal_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: vertical_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: diagonal_right_pattern!, valid_lines: [1,4]).count
+total_1 = total_1 + findOverlappingMatches(in: String(input.reversed()), regex: diagonal_left_pattern!, valid_lines: [1,4]).count
 
 
 
 print("Part 1 \(total_1)")
 
+var total_2 = 0
+let cross_pattern = try? Regex("M.S(.|\n){\(move-1)}A(.|\n){\(move-1)}M.S")
+let cross_pattern_invert = try? Regex("S.S(.|\n){\(move-1)}A(.|\n){\(move-1)}M.M")
 
-let cross_pattern = try? Regex("M.SX(.|\n){\(move)}M(.|\n){\(move)}A(.|\n){\(move)}S")
+total_2 = total_2 + findOverlappingMatches(in: input, regex: cross_pattern!, valid_lines: [3]).count
+total_2 = total_2 + findOverlappingMatches(in: input, regex: cross_pattern_invert!, valid_lines: [3]).count
+total_2 = total_2 + findOverlappingMatches(in: String(input.reversed()), regex: cross_pattern!, valid_lines: [3]).count
+total_2 = total_2 + findOverlappingMatches(in: String(input.reversed()), regex: cross_pattern_invert!, valid_lines: [3]).count
 
+print("Part 2 \(total_2)")
